fe34acdc6e9edbbddbd09d82c8dee5882d03c6fc
goldog <goldog83@gmail.com>
2020-12-30 14:41:54 +0900

    미구현 부분 추가

--- src/main/java/com/mctak/springjwtauth/models/Role.java
+++ src/main/java/com/mctak/springjwtauth/models/Role.java
@@ -11,7 +11,7 @@ public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     @Enumerated(EnumType.STRING)
     @Column(length = 20)
--- src/main/java/com/mctak/springjwtauth/payload/response/JwtResponse.java
+++ src/main/java/com/mctak/springjwtauth/payload/response/JwtResponse.java
@@ -6,13 +6,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class JwtResponse {
-    private final String type = "Bearer";
-    private String token;
+    private String accessToken;
+    private String tokenType = "Bearer";
     private Long id;
-	private String username;
-	private String email;
-	private List<String> roles;
-    
+    private String username;
+    private String email;
+    private List<String> roles;
+
+    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
+        this.accessToken = accessToken;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
 }
